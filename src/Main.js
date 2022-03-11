100b9ba586b05930155ef9c2dc579041c91a237f
Ridho Nur Rohman Wijaya <ridhonurrohmanwijaya@gmail.com>
2022-03-11 10:30:06 +0700

    Mau membuat modal

--- src/Main.js
+++ src/Main.js
@@ -1,77 +1,140 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 const useStyle = makeStyles((theme) => ({
-    root: {
-      minHeight: '100vh',
-      background: 'green',
-      width: '100%',
-      overflowY: 'auto',
-    },
-    listContainer: {
-      display: 'flex',
-    },
-  }));
+  root: {
+    minHeight: '100vh',
+    background: 'green',
+    width: '100%',
+    overflowY: 'auto',
+  },
+  listContainer: {
+    display: 'flex',
+  },
+}));
 
 export default function Ujian() {
   const [data, setData] = useState([])
   const [edit, setEdit] = useState(null)
-  useEffect(()=>{
+  useEffect(() => {
     getData()
-  },[])
+  }, [])
   //  untuk mengambil data
-  const getData = () =>{
+  const getData = () => {
     axios.get('http://localhost:3001/trello')
-    .then(hasil=>setData(hasil.data))
+      .then(hasil => setData(hasil.data))
   }
   // untuk memasukkan data
-  const handleSubmit = (e)=>{
-    e.preventDefault() 
+  const handleSubmit = (e) => {
+    e.preventDefault()
     const value = e.target.submit.value
-    axios.post('http://localhost:3001/trello', {name: value})
-    .then(()=>{
-      getData()
-      e.target.submit.value=''
-    })
+    axios.post('http://localhost:3001/trello', { name: value })
+      .then(() => {
+        getData()
+        e.target.submit.value = ''
+      })
   }
   // untuk menghapus data
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     axios.delete(`http://localhost:3001/trello/${id}`)
-    .then(()=>getData())
+      .then(() => getData())
   }
+
   // untuk menyimpan data yang sudah diedit
-  const handleEdit = (e) =>{
+  const handleEdit = (e) => {
     e.preventDefault()
-    axios.patch(`http://localhost:3001/trello/${data[edit].id}`, {name: e.target.save.value})
-    .then(()=>{
-      getData()
-      setEdit(null)
-    })
+    axios.patch(`http://localhost:3001/trello/${data[edit].id}`, { name: e.target.save.value })
+      .then(() => {
+        getData()
+        setEdit(null)
+      })
   }
-  return (
-      // submit data
+
+  const cobaKartu = () => {
+    return (<div>
+      <form onSubmit={handleSubmit}>
+        <div>Aplikasi Trello</div>
+        <input name='submit'></input>
+        <button>Tambah</button>
+      </form>
       <div>
-        <form onSubmit={handleSubmit}>
-          <div>Aplikasi Trello</div>
-          <hr></hr>
-          <input name='submit'></input>
-          <button>Tambah</button>
-        </form>
-        <div>
-          {data.map((value, index)=>{
-            return (
-              edit === index?
+        {data.map((value, index) => {
+          return (
+            edit === index ?
               <form key={index} onSubmit={handleEdit}>
-                <input name='save' defaultValue={value.name}/> <button>save</button>
+                <input name='save' defaultValue={value.name} /> <button>save</button>
               </form>
-            : <div key={index}>{value.name} 
-              <button onClick={ () => handleDelete(value.id)}> hapus</button> 
-              <button onClick={ () => setEdit(index)}> edit</button>
-            </div>)
-          })}
-        </div>
+              : <div key={index}>{value.name} <button onClick={() => handleDelete(value.id)}> hapus</button> <button onClick={() => setEdit(index)}> edit</button>
+              </div>)
+        })}
       </div>
-      
+    </div>)
+  }
+
+  const cobaKlik = (e) => {
+    e.preventDefault()
+    console.log("aa")
+  }
+
+  return (
+    // submit data
+    <div>
+      Aplikasi Trello
+      <form>
+        <button onClick={cobaKlik}>Tambah Kartu</button>
+      </form>
+      <hr></hr>
+      {cobaKartu()}
+
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Note</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Form.Label htmlFor="note">Tambahkan Note</Form.Label>
+            <Form.Control type="text" id="note" name="submit" />
+            <div>
+              {data.map((value, index) => {
+                return (
+                  edit === index ?
+                    <form key={index} onSubmit={handleEdit}>
+                      <input name='save' defaultValue={value.name} /> <button>save</button>
+                    </form>
+                    : <div key={index}>{value.name} <button onClick={() => handleDelete(value.id)}>hapus</button> <button onClick={() => setEdit(index)}> edit</button>
+                    </div>)
+              })}
+            </div>
+          </div>
+          </form>
+
+        </Modal.Body>
+      </Modal.Dialog>
+
+      {/* <form onSubmit={handleSubmit}>
+        <div>Aplikasi Trello</div>
+        <input name='submit'></input>
+        <button>Tambah</button>
+      </form>
+      <div>
+        {data.map((value, index) => {
+          return (
+            edit === index ?
+              <form key={index} onSubmit={handleEdit}>
+                <input name='save' defaultValue={value.name} /> <button>save</button>
+              </form>
+              : <div key={index}>{value.name}
+                <button onClick={() => handleDelete(value.id)}> hapus</button>
+                <button onClick={() => setEdit(index)}> edit</button>
+              </div>)
+        })}
+      </div> */}
+    </div >
+
   )
 }
